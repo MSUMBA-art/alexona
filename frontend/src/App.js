2d042e99752e08a17d972e7815eb185344536e07
ALEXANDER <msumba83@gmail.com>
2023-04-02 00:29:25 +0200

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">alexona</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
