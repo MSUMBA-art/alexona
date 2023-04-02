c04446d948cce58f6764c4a2e3d7d0a8d2e3bddb
ALEXANDER <msumba83@gmail.com>
2023-04-02 15:49:30 +0200

    created data file and add products , styled it.

---
+++ frontend/src/data.js
@@ -0,0 +1,54 @@
+const data = {
+  products: [
+    {
+      name: 'Nike Slim shirt',
+      slug: 'nike-slim-shirt',
+      category: 'Shirts',
+      image: '/images/p4.jpg',
+      price: 120,
+      countInStore: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+      name: 'Puma Slim shirt',
+      slug: 'puma-slim-shirt',
+      category: 'Shirts',
+      image: '/images/p3.jpg',
+      price: 120,
+      countInStore: 10,
+      brand: 'Puma',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+      name: 'Calvin Klein Slim shirt',
+      slug: 'ck-slim-shirt',
+      category: 'Shirts',
+      image: '/images/p2.jpg',
+      price: 120,
+      countInStore: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+      name: 'Adidas Slim shirt',
+      slug: 'adidas-slim-shirt',
+      category: 'Shirts',
+      image: '/images/p1.jpg', // 679px x 839px
+      price: 120,
+      countInStore: 10,
+      brand: 'Adidas',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+  ],
+};
+
+export default data
